01ca094c452d4ab17c8f66b5b4b8dd024a0f4d4f
Adam Gajzlerowicz <nelf86@gmail.com>
2020-01-18 22:09:17 +0100

    interface allows to delete env

--- web-interface/index.py
+++ web-interface/index.py
@@ -10,17 +10,20 @@ CORS(app)
 @app.route('/', methods=["GET"])
 def get():
     result = subprocess.run(['helm', 'list', '-o', 'json'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result_json = json.loads(result)
+    output_dict = [x for x in result_json if x['name'].startswith("git-branch")]
 
-    return jsonify(json.loads(result))
+    return json.dumps(output_dict)
 
 
-@app.route('/<name>', methods=["DELETE"])
+@app.route('/', methods=["DELETE"])
 def delete():
     name = request.args.get("name")
+    print(name)
     if not name.startswith("git-branch"):
         abort(404)
 
-    result = subprocess.run(["helm", "uninstall", name], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    return jsonify(json.loads(result))
+    subprocess.run(["helm", "uninstall", name], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
